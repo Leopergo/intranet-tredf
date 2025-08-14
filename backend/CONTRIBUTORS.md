26077cfe8be81482595e6a86c4c12b03b1faf8a6
Leopergo <leonardogonc@gmail.com>
2025-08-14 14:18:35 -0300

    Exercício 14: Commit inicial

---
+++ backend/CONTRIBUTORS.md
@@ -0,0 +1,3 @@
+# Contributors
+
+- leonardo [leonardo.goncalves@tre-df.jus.br]
---
+++ frontend/packages/volto-tredf-intranet/src/index.js
@@ -0,0 +1,28 @@
+const applyConfig = (config) => {
+  config.settings.isMultilingual = false;
+  config.settings.supportedLanguages = ['pt-br'];
+  config.settings.defaultLanguage = 'pt-br';
+  config.settings = {
+    ...config.settings,
+    isMultilingual: false,
+    supportedLanguages: ['pt-br'],
+    defaultLanguage: 'pt-br',
+    image_crop_aspect_ratios: [
+      {
+        label: '16:9',
+        ratio: 16 / 9,
+      },
+      {
+        label: '4:3',
+        ratio: 4 / 3,
+      },
+      {
+        label: '1:1',
+        ratio: 1,
+      },
+    ],
+  };
+  return config;
+};
+
+export default applyConfig;
