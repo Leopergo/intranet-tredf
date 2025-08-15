523d1e9fd531e876094ee42acd75dacd39dc307f
Leopergo <leonardogonc@gmail.com>
2025-08-15 14:31:47 -0300

    Exercício 23: Visão para o tipo Área

--- frontend/packages/volto-tredf-intranet/src/index.js
+++ frontend/packages/volto-tredf-intranet/src/index.js
@@ -1,26 +1,27 @@
+import AreaView from './components/Views/AreaView';
+
 const applyConfig = (config) => {
   config.settings.isMultilingual = false;
   config.settings.supportedLanguages = ['pt-br'];
   config.settings.defaultLanguage = 'pt-br';
-  config.settings = {
-    ...config.settings,
-    isMultilingual: false,
-    supportedLanguages: ['pt-br'],
-    defaultLanguage: 'pt-br',
-    image_crop_aspect_ratios: [
-      {
-        label: '16:9',
-        ratio: 16 / 9,
-      },
-      {
-        label: '4:3',
-        ratio: 4 / 3,
-      },
-      {
-        label: '1:1',
-        ratio: 1,
-      },
-    ],
+  config.settings.image_crop_aspect_ratios = [
+    {
+      label: '16:9',
+      ratio: 16 / 9,
+    },
+    {
+      label: '4:3',
+      ratio: 4 / 3,
+    },
+    {
+      label: '1:1',
+      ratio: 1,
+    },
+  ];
+  // Registra Visoes padrao para tipos de conteúdo
+  config.views.contentTypesViews = {
+    Area: AreaView,
+    ...config.views.contentTypesViews,
   };
   return config;
 };
